91208cb29b312c091fe3ea216520227d7251dbcc
SalriS <salris@naver.com>
2023-08-19 18:14:53 +0900

    Create 3.data.c

---
+++ Project1/Project1/3.data.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("%d\n", 12);
+	printf("%d\n", 014);
+	printf("%d\n", 0xc);
+	printf("%.1lf\n",1e6);
+	printf("%.9lf\n", 3.14e-5);
+	printf("%c은%s입니다.\n", '1', "first");
+}
